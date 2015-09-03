cee5012ee08547d1cb63764f28272d8d93de8788
Vietworm <0xgi@users.noreply.github.com>
2015-09-03 14:47:11 +0700

    Fix get content-type, download file function

--- Tools/bkhn.py
+++ Tools/bkhn.py
@@ -1,7 +1,9 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 # Language: Python
 # Development Environment: Windows 8.1
-# Author: 0xgi
-# Description: Download document file from URL: http://www.cse.hcmut.edu.vn/~hungnq/courses/ 
+# Author: Vietworm
+# Description: Download document file from URL: http://www.cse.hcmut.edu.vn/~hungnq/courses/
 
 # Dependency: requests, scrapy, urllib
 # Installation: pip install <package>
@@ -9,67 +11,90 @@
 import requests
 import urllib
 import scrapy
+import mimetypes
 URI = 'http://www.cse.hcmut.edu.vn/~hungnq/courses/501120/'
 
+
 def html(url):
-	r = requests.get(url)
-	result = []
-	result.extend([r.status_code,r.headers['content-type'],r.text,r.encoding])
-	return result
+    r = requests.get(url)
+    result = []
+    result.extend([r.status_code, r.headers['content-type'], r.text,
+                  r.encoding])
+    return result
+
 
 def getFileInfo(url):
-	r = urllib.urlopen(url)
-	http_message = r.info()
-	type = http_message.type
-	return type
-	
+    r = urllib.urlopen(url)
+    http_message = r.info()
+    type = http_message.type
+    return type
+
+
 # Check Content Type from URL
-# isFile return contains 'application', isDirectory return contains 'text/html'	
+# isFile return contains 'application', isDirectory return contains 'text/html'....
 
-# FIXME: Using content-type for check file type is bad idea.
-# Request as well as file download :( Long time request for check file type.
-def isFile(contentType):
-	if not "text/html" in contentType:
-		return True
-	else:
-		return False		
-		
-# Check has content from new URL		
+# Get file type using mimetypes modules
+
+def isFile(url):
+    if mimetypes.guess_type(url, strict=True)[0] == None:
+        return False
+    else:
+        return True
+
+
+# Check has content from new URL........
+
 def hasContent(url):
-	return
-	
-def scrapyExtract(url):
-	r = html(url) # Return array from requests [status_code, content-type, text/plain, encoding]
-	sel = scrapy.Selector(text=r[2], type="html")
-	return sel.xpath('//td//@href').extract()[1:] # Find @href value from text/plain.
+    return
 
-def download(path):
-	d = urllib.URLopener()
-	d.retrieve(path)
-	
+
+def scrapyExtract(url):
+    r = html(url)  # Return array from requests [status_code, content-type, text/plain, encoding]
+    sel = scrapy.Selector(text=r[2], type='html')
+    return sel.xpath('//td//@href').extract()[1:]  # Find @href value from text/plain.
+
+
+def download(path, fileName):
+    d = urllib.URLopener()
+    d.retrieve(path, fileName)
+
+
 def hilite(string, status, bold):
     attr = []
     if status:
+
         # green
+
         attr.append('32')
     else:
+
         # red
+
         attr.append('31')
     if bold:
         attr.append('1')
-    return '\x1b[%sm%s\x1b[0m' % (';'.join(attr), string)	
-	
-# print hilite('crawler','green',False)	
+    return '\x1b[%sm%s\x1b[0m' % (';'.join(attr), string)
+
+
+def main(location):
+    print 'Download processing...'
+    for i in location:
+        url = URI + i
+        if isFile(url):
+            try:
+                download(url, i)
+                print '(Complete) - ' + url
+            except IOError:
+                print '####URL: ' + url + " can't download"
+        else:
+            subLocation = scrapyExtract(url)
+            main(subLocation)
+
+
+# print hilite('crawler','green',False)....
 
 # Recursive funtion load directory and download file.
+
 location = scrapyExtract(URI)
 print 'URI: found ' + str(len(location)) + ' items\n'
-for i in location:
-	type = getFileInfo(URI + i)
-	if isFile(type): 
-		print '\nFile: ' + i
-	else:
-		print '\n#Directory: '+ i
-	
-
-# .pptx return Content-type: text/plain	
+main(location)
