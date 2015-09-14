ed19a530e40df909111c75cb584615595d6e46e5
0xgi <lephuhai0511@gmail.com>
2015-09-15 00:30:52 +0700

    # Fibonacci

---
+++ training/fibonacci.py
@@ -0,0 +1,26 @@
+﻿# -*- coding: utf-8 -*-
+
+"""
+Tìm số fibonacci thứ n
+"""
+def fibo(n):
+	if (n == 0 or n == 1):
+		return 1
+	else:
+		return fibo(n - 1) + fibo(n - 2)
+		
+x = fibo(6)
+
+print x
+
+"""
+Dãy số fibonacci nhỏ hơn 100
+"""
+
+def fibo2():
+	a, b = 0, 1
+	while b < 100:
+		print b,
+		a, b = b, a + b
+		
+fibo2()
