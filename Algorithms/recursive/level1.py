33e9fec64ce8ae6f07ebd00ea9b7a8ea6393c511
0xgi <lephuhai0511@gmail.com>
2015-09-16 01:29:48 +0700

    # Update tail recursive function

--- Algorithms/recursive/level1.py
+++ Algorithms/recursive/level1.py
@@ -7,12 +7,18 @@ Phân tích:
  - Xác định điều kiện dừng:  Nếu n == 1 thì return 1
  - Công thức: S(n) = S(n - 1) + n
 '''
+
+# Sử dụng phương pháp đệ quy
+
+
 def S(n):
     if n == 1:
         return 1
     return S(n - 1) + n
 
 # Không sử dụng đệ quy
+
+
 def Si(n):
     tong = 0
     for i in range(n + 1):
@@ -20,5 +26,15 @@ def Si(n):
     return tong
 
 
-print "Ket qua de quy cua {} la: {}".format(4,S(4))
-print "Ket qua de quy cua {} la: {}".format(5,Si(5))
+# Sử dụng đệ quy đuôi (Tail recursive)
+
+
+def tail(n, x=1):
+    if n == 1:
+        return x
+    return tail(n - 1, x + n)
+
+
+print "Kết quả {}! = {}".format(4, S(4))
+print "Kết quả {}! = {}".format(5, Si(5))
+print "Kết quả {}! = {}".format(5, tail(5))
